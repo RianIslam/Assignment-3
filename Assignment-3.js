dbde635be6544f46f195df9e5c3b46f07f5311e8
RianIslam <rianislamdr@gmail.com>
2021-01-22 06:02:40 +0600

    add if else

--- Assignment-3.js
+++ Assignment-3.js
@@ -1,62 +1,67 @@
-// function kilometerToMeter(valu){
-//     var kilo = valu* 1000;
-//     return kilo;
-// }
-// var result = kilometerToMeter(84);
-// console.log(result);
+function kilometerToMeter(valu){
+    if(valu==0){
+        console.log("false")
+    }else{
+
+        var kilo = valu* 1000;
+        return kilo;
+    }
+}
+var result = kilometerToMeter(0);
+console.log(result);
 
 
 
 
-// function budgetCalculator(watchNum,mobNum,lapNum)
-// {
-//     var allAmount = watchNum * 50+mobNum * 100+lapNum * 500;
-//     return allAmount;
-// }
-//  var result =budgetCalculator(2,2,2);
-//  console.log(result)
-
-
-
-
-
-
-
-
-
-
-
-// function hotelCost(day){
-//     var stay = 0;
-//     if(day <= 10){
-//         stay = day * 100;
-
-//     }
-//     else if(day <= 20){
-//         var firstCase = 10* 100;
-//         var remaining = day - 10;
-//         var secondCase = remaining*80;
-//         stay = firstCase+secondCase;
-//     }
-//     else{
-//         var firstCase = 10*100;
-//         var remaining = day - 10;
-//         var secondCase = 10*80;
-//         var thirdCase = remaining* 50;
-//         stay = firstCase+secondCase+thirdCase;
-//     }
-//     return stay;
-// }
-
-// var result = hotelCost(21);
-
-// console.log(result);
+function budgetCalculator(watchNum,mobNum,lapNum)
+{
+    var allAmount = watchNum * 50+mobNum * 100+lapNum * 500;
+    return allAmount;
+}
+ var result =budgetCalculator(2,2,2);
+ console.log(result)
 
 
 
 
 
-function something(frndNames){
+
+
+
+
+
+
+function hotelCost(day){
+    var stay = 0;
+    if(day <= 10){
+        stay = day * 100;
+
+    }
+    else if(day <= 20){
+        var firstCase = 10* 100;
+        var remaining = day - 10;
+        var secondCase = remaining*80;
+        stay = firstCase+secondCase;
+    }
+    else{
+        var firstCase = 10*100;
+        var remaining = day - 10;
+        var secondCase = 10*80;
+        var thirdCase = remaining* 50;
+        stay = firstCase+secondCase+thirdCase;
+    }
+    return stay;
+}
+
+var result = hotelCost(21);
+
+console.log(result);
+
+
+
+
+
+function megaFriend(frndNames){
     var highLenth = frndNames[0];
 
     for (var i = 0; i<frndNames.length; i++)
@@ -73,5 +78,5 @@ function something(frndNames){
 
 
 var frndNames = ["Habib","Raja","Shimul","TitumirRohoman","mynameisjabbarkhan"];
-var result = something(frndNames)
+var result = megaFriend(frndNames)
 console.log(result);
